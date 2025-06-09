be99f4cfb1e4a08e4f9b127624126c4dd98f4fd5
Krealer <Krealerkimizuki@gmail.com>
2025-06-09 19:45:57 +0100

    Add blueprint crafting system

--- scripts/craft_state.js
+++ scripts/craft_state.js
@@ -1,3 +1,13 @@
 export const craftState = {
   lastCrafted: null,
+  unlockedBlueprints: new Set(),
 };
+
+export function unlockBlueprint(id) {
+  craftState.unlockedBlueprints.add(id);
+  document.dispatchEvent(new CustomEvent('blueprintUnlocked', { detail: id }));
+}
+
+export function isBlueprintUnlocked(id) {
+  return craftState.unlockedBlueprints.has(id);
+}
