a9dbdf93eb54ce8c9fb4d09d6f70be96fddc8095
Krealer <Krealerkimizuki@gmail.com>
2025-06-12 13:42:16 +0100

    fix extra closing brace in mapLoader

--- scripts/mapLoader.js
+++ scripts/mapLoader.js
@@ -96,7 +96,6 @@ export async function loadMap(name) {
           }
         }
       }
-    }
   } catch (err) {
     console.error(err);
     showError(err.message || `Failed to load map ${name}`);
