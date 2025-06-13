dc198e3315b4e7ea38f2deaedd2290595c62c3ea
Krealer <Krealerkimizuki@gmail.com>
2025-06-13 15:08:13 +0100

    fix data load paths

--- scripts/item_loader.js
+++ scripts/item_loader.js
@@ -7,7 +7,7 @@ let items = {};
 export async function loadItems() {
   if (Object.keys(items).length) return items;
   try {
-    const data = await loadJson('data/items.json');
+    const data = await loadJson('../data/items.json');
     items = { ...data, ...itemData };
   } catch (err) {
     items = { ...itemData };
