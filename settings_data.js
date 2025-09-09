651c9ebf8667533729844a4dfabdb0b42dab97e5
Krealer <Krealerkimizuki@gmail.com>
2025-09-09 19:35:25 +0100

    Revert "Merge pull request #498 from Krealer/codex/add-zoom-feature-to-settings"
    This reverts commit d55a006aa4f951cb209b9d3dbb650c25f56217de, reversing
    changes made to a9dbd6b4796c22b01bd913411a0acfb6e0ffa027.

--- settings_data.js
+++ settings_data.js
@@ -27,10 +27,6 @@ export default {
     default: true,
     description: 'Type out dialogue text'
   },
-  zoom: {
-    default: true,
-    description: 'Enable zoom controls'
-  },
   language: {
     default: 'en',
     description: 'UI language code'
